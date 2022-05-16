0784155c922faff3bb5d1ee2f2a104308ae67ae7
tickbh <tickdream125@hotmail.com>
2022-05-16 13:31:18 +0800

    luacjson luasocket

--- luacjson/src/lib.rs
+++ luacjson/src/lib.rs
@@ -3,5 +3,27 @@ extern crate td_rlua;
 
 #[allow(improper_ctypes)]
 extern "C" {
-    pub fn luaopen_cjson(L : *mut td_rlua::lua_State);
+    pub fn luaopen_cjson(L : *mut td_rlua::lua_State) -> libc::c_int ;
+}
+
+extern "C" fn safe_luaopen_cjson(lua: *mut td_rlua::lua_State) -> libc::c_int {
+    unsafe {
+        luaopen_cjson(lua)
+    }
+}
+
+/// custom lua load func
+extern "C" fn load_func(lua: *mut td_rlua::lua_State) -> libc::c_int {
+    let path:String = td_rlua::LuaRead::lua_read(lua).unwrap_or(String::new());
+    if &path == "cjson" {
+        unsafe {
+            td_rlua::lua_pushcfunction(lua, safe_luaopen_cjson);
+        }
+        return 1;
+    }
+    return 0;
+}
+
+pub fn enable_cjson(lua : &mut td_rlua::Lua) {
+    lua.add_lualoader(load_func);
 }
--- luasocket/src/lib.rs
+++ luasocket/src/lib.rs
@@ -2,5 +2,27 @@ extern crate td_rlua;
 
 #[allow(improper_ctypes)]
 extern "C" {
-    pub fn luaopen_socket_core(L : *mut td_rlua::lua_State);
+    pub fn luaopen_socket_core(L : *mut td_rlua::lua_State) -> libc::c_int;
+}
+
+extern "C" fn safe_luaopen_socket_core(lua: *mut td_rlua::lua_State) -> libc::c_int {
+    unsafe {
+        luaopen_socket_core(lua)
+    }
+}
+
+/// custom lua load func
+extern "C" fn load_func(lua: *mut td_rlua::lua_State) -> libc::c_int {
+    let path:String = td_rlua::LuaRead::lua_read(lua).unwrap_or(String::new());
+    if &path == "socket.core" || &path == "luasocket" || &path == "socket" {
+        unsafe {
+            td_rlua::lua_pushcfunction(lua, safe_luaopen_socket_core);
+        }
+        return 1;
+    }
+    return 0;
+}
+
+pub fn enable_socket_core(lua : &mut td_rlua::Lua) {
+    lua.add_lualoader(load_func);
 }
